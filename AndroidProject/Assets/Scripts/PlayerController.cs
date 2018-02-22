98f197ac4ffdf0a69112da7e976e81950eb805b9
Blackisher <lukasz.matejuk@gmail.com>
2018-02-23 00:13:37 +0100

    Added player. He has Box collider around him ( with material from walls) and very low center of mass ( so he will not fly away)  and mass 10 times bigger then ball. Center of mass added by script.

---
+++ AndroidProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Vector3 com;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = com;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
