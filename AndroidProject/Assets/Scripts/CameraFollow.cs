6cdbd87261132a07bed30e4eb5bb02a3e06391e8
Blackisher <lukasz.matejuk@gmail.com>
2018-02-24 13:06:32 +0100

    rotation of camera is working

--- AndroidProject/Assets/Scripts/CameraFollow.cs
+++ AndroidProject/Assets/Scripts/CameraFollow.cs
@@ -10,16 +10,12 @@ public class CameraFollow : MonoBehaviour {
     public float lerpRate;
     public bool gameOver;
 
-    // Use this for initialization
     void Start () {
         offset = player.transform.position - transform.position;
         offsetMagnitute = offset.magnitude;
         offset = offset.normalized;
-
-
     }
 	
-	// Update is called once per frame
 	void Update () {
         if (!gameOver)
         {
@@ -30,40 +26,18 @@ public class CameraFollow : MonoBehaviour {
     void Follow()
     {
         Quaternion rotation = player.transform.rotation;
-
-        if (!transform.rotation.Equals(rotation))
-        {
-            //still problem - its just rotating over and over and not just a little
-            //offset = rotation * offset;
-            //Vector3 pos = transform.position;
-            //Vector3 target = player.transform.position - offset * offsetMagnitute;
-            //pos = Vector3.Lerp(pos, target, lerpRate * Time.deltaTime);
-            //transform.position = pos;
-        }
-        
-        
-        
-        
-        //Notepad.....
-
-
-
-        //transform.position = transform.position - change;
-        //player.transform.position
-        /*
-        //current position of camera
+        //offset is normalized
+        //so new offset too
+        Vector3 newOffset = rotation * offset;
+        //camera current position
         Vector3 pos = transform.position;
-        System.Console.WriteLine("pos:" + pos);
-        Vector3 change = new Vector3(Input.acceleration.x, Input.acceleration.y, 0);
-        System.Console.WriteLine("change:" + change);
-        offset = (offset + change).normalized * offset.magnitude;
-        System.Console.WriteLine("offset:" + offset);
-        Vector3 target = player.transform.position - offset;
-        System.Console.WriteLine("target:" + target);
+        //where camera should go
+        Vector3 target = player.transform.position - newOffset * offsetMagnitute;
+        //new position for camera without shaking
         pos = Vector3.Lerp(pos, target, lerpRate * Time.deltaTime);
-        System.Console.WriteLine("pos:" + pos);
+        //assign new possition.
         transform.position = pos;
-
-    */
+        //turn to player
+        transform.LookAt(player.transform.position);
     }
 }
