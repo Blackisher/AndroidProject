9280b50fd7dd44a9371837ee5e4ee11b938473de
Владимир <trenzenokv@gmail.com>
2018-03-18 14:11:58 +0200

    Camera + Jumping fixed
    Camera added for each player. Jumping fixed (start pos of ball
    moved...).

--- AndroidProject/Assets/Scripts/Player_NetworkSetUp.cs
+++ AndroidProject/Assets/Scripts/Player_NetworkSetUp.cs
@@ -5,19 +5,25 @@ using UnityEngine.Networking;
 
 public class Player_NetworkSetUp : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    [SerializeField] Camera PlayerCamera;
+    [SerializeField] CameraFollow PlayerCameraFollow;
+
+
+    // Use this for initialization
+    void Start()
+    {
         if (isLocalPlayer)
         {
             //GameObject.Find("Main Camera").SetActive(false);
-           // GetComponent<CharacterController>().enabled = true;
+            // GetComponent<CharacterController>().enabled = true;
             //GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl>().enabled = true;
             GetComponent<PlayerController>().enabled = true;
             GetComponent<BoxCollider>().enabled = true;
             //GetComponent<Animator>().enabled = true;
-
+            PlayerCamera.enabled = true;
+            PlayerCameraFollow.enabled = true;
         }
-	}
-	
+    }
 
 }
